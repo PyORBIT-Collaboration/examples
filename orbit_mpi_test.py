276c8ae39d794db2ffc51f079753c723c124f1bb
Andrei Shishlo <shishlo@hotmail.com>
2007-11-15 14:18:18 +0000

    it will be removed

--- orbit_mpi_test.py
+++ orbit_mpi_test.py
@@ -1,10 +1,36 @@
-import sys
 import orbit_mpi
+import sys
 
-cpu  = orbit_mpi.cpu()
-rank = orbit_mpi.world_rank()
-size = orbit_mpi.world_size()
-tm = orbit_mpi.time()
-print "rank=",rank," size=",size, " name=", cpu," time=",tm
+cpu  = orbit_mpi.MPI_Get_processor_name()
+rank = orbit_mpi.MPI_Comm_rank(orbit_mpi.mpi_comm.MPI_COMM_WORLD)
+size = orbit_mpi.MPI_Comm_size(orbit_mpi.mpi_comm.MPI_COMM_WORLD)
+tm = orbit_mpi.MPI_Wtime()
+tk = orbit_mpi.MPI_Wtick()
+print "rank=",rank," size=",size, " name=", cpu," time=",tm," tick=",tk
 
-orbit_mpi.finalize("Error Message!")
+comm_world = orbit_mpi.mpi_comm.MPI_COMM_WORLD
+comm = orbit_mpi.mpi_comm.MPI_Comm()
+orbit_mpi.MPI_Comm_dup(comm_world,comm)
+orbit_mpi.MPI_Comm_set_name(comm,"my_communicator")
+print "comm_world=",orbit_mpi.mpi_comm.MPI_COMM_WORLD
+print "comm_self=",orbit_mpi.mpi_comm.MPI_COMM_SELF
+print "comm=",comm
+name = orbit_mpi.MPI_Comm_get_name(comm)
+print "name comm=",name
+
+group1 = orbit_mpi.mpi_group.MPI_Group()
+group2 = orbit_mpi.mpi_group.MPI_Group()
+orbit_mpi.MPI_Group_incl(group1,[-5.1,2,3],group2)
+
+sys.exit(1)
+
+count = 0
+while(1 < 2):
+	count += 1
+	orbit_mpi.MPI_Comm_set_name(comm,"my_communicator"+str(count))
+	name = orbit_mpi.MPI_Comm_get_name(comm)
+	if(count % 10000 == 0):
+		print "i=",count
+
+#orbit_mpi.finalize("Error Message!")
+orbit_mpi.finalize()
