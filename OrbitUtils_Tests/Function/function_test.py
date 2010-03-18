804dd1cba59b9369a505e5b92229f4e22586a2d5
Andrei Shishlo <shishlo@hotmail.com>
2010-03-18 23:23:20 +0000

    Next step, but it is not the end.

--- OrbitUtils_Tests/Function/function_test.py
+++ OrbitUtils_Tests/Function/function_test.py
@@ -2,7 +2,7 @@ import sys
 from orbit_utils import Function
 
 f = Function()
-f.add(0.,1.0)
+f.add(0.,0.5)
 f.add(1.,1.0)
 print "size=",f.getSize()
 for i in range(f.getSize()):
@@ -11,3 +11,7 @@ for i in range(f.getSize()):
 	print "==== i=",i
 	print "x=",x," y=",y
 	print "(x,y)=",f.xy(i) 
+	
+f.dump()
+f.normalize()
+f.dump()
