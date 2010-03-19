159de3e61f6356031c9385014836ba90e996d416
Andrei Shishlo <shishlo@hotmail.com>
2010-03-19 21:21:06 +0000

    Next step, but it is not the end.

--- OrbitUtils_Tests/Function/function_test.py
+++ OrbitUtils_Tests/Function/function_test.py
@@ -2,16 +2,14 @@ import sys
 from orbit_utils import Function
 
 f = Function()
-f.add(0.,0.5)
-f.add(1.,1.0)
-print "size=",f.getSize()
-for i in range(f.getSize()):
-	x = f.x(i)
-	y = f.y(i)
-	print "==== i=",i
-	print "x=",x," y=",y
-	print "(x,y)=",f.xy(i) 
+for i in range(10):
+	f.add(1.0*i,0.5*i)
 	
+#f.add(5.1,1000.)
+
 f.dump()
-f.normalize()
+print "const step=",f.setConstStep(1)
+
+print "y(5.5)=",f.getY(5.5)
+print "norm=",f.normalize()
 f.dump()
