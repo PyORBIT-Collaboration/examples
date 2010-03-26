8514e573b143d07a28fbbd7668fae50ec3885986
Andrei Shishlo <shishlo@hotmail.com>
2010-03-26 21:53:41 +0000

    Next step, but it is not the end.

--- OrbitUtils_Tests/Function/function_test.py
+++ OrbitUtils_Tests/Function/function_test.py
@@ -1,15 +1,49 @@
 import sys
+import math
+
 from orbit_utils import Function
+from orbit_utils import SplineCH
 
 f = Function()
-for i in range(10):
-	f.add(1.0*i,0.5*i)
+
+def FF(x):
+	return math.sin(x)
+
+def FFP(x):
+	return math.cos(x)
+
+n = 40
+step = 2*math.pi/n
+for i in range(n):
+	x = step*i+0.1*((1.0*i)/n)**2;
+	y = FF(x)
+	f.add(x,y)
+
+f.dump()
+
+spline = SplineCH()
+spline.compile(f)
 	
-#f.add(5.1,1000.)
+spline.dump()
 
-f.dump()
-print "const step=",f.setConstStep(1)
+print "================"
+n = 100
+step = 0.8*(f.getMaxX() - f.getMinX())/(n-1)
+y_dev_max = 0.
+yp_dev_max = 0.
+for j in range(n):
+	x = f.getMinX() + j*step
+	y = FF(x)
+	yp = FFP(x)
+	ys = math.fabs(spline.getY(x) - y)
+	yps = math.fabs(spline.getYP(x) - yp)
+	if(y_dev_max < ys): 
+		#print "debug x=",x," dev y=",ys," y=",y
+		y_dev_max = ys
+	if(yp_dev_max < yps): 
+		#print "debug x=",x," dev yp=",yps," yp=",yp
+		yp_dev_max = yps
+	
+print " deviation y max =",y_dev_max
+print " deviation yp max =",yp_dev_max
 
-print "y(5.5)=",f.getY(5.5)
-print "norm=",f.normalize()
-f.dump()
