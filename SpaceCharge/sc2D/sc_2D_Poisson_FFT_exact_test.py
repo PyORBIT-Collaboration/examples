0d3a5d20e12aedebf8651052f1e227491bc2ebe5
Andrei Shishlo <shishlo@hotmail.com>
2012-12-06 15:04:32 +0000

    The FFT solver exact test for string like and point like charge. The gradient added.

--- SpaceCharge/sc2D/sc_2D_Poisson_FFT_exact_test.py
+++ SpaceCharge/sc2D/sc_2D_Poisson_FFT_exact_test.py
@@ -22,9 +22,9 @@ from spacecharge import PoissonSolverFFT2D
 
 print "Start."
 
-sizeX = 10
-sizeY = 10
-xMin = -3.0
+sizeX = 200
+sizeY = 200
+xMin = -5.0
 xMax = +5.0
 yMin = -5.0
 yMax = +5.0
@@ -53,16 +53,25 @@ solver.findPotential(gridRho,gridPhi)
 x0 = gridRho.getGridX(ind_x)
 y0 = gridRho.getGridY(ind_y)
 
+x_step = (gridRho.getMaxX() - gridRho.getMinX())/(sizeX-1) 
+y_step = (gridRho.getMaxY() - gridRho.getMinY())/(sizeY-1) 
+step_total = math.sqrt(x_step**2+y_step**2)
+
 max_dev = 0.
 max_diff_ix = -1
 max_diff_iy = -1
+max_grad_diff = 0.
+max_grad_diff_ix = -1
+max_grad_diff_iy = -1
 max_diff_phy = 0
 for ix in range(sizeX):
 	for iy in range(sizeY):
 		if(ix != ind_x or ind_y != iy):
 			x = gridRho.getGridX(ix)
 			y = gridRho.getGridY(iy)
-			phi_th = - charge*math.log(math.sqrt((x-x0)**2+(y-y0)**2))
+			dist = math.sqrt((x-x0)**2+(y-y0)**2)
+			#-------value---------------------------
+			phi_th = - charge*math.log(dist)
 			phi = gridPhi.getValueOnGrid(ix,iy)
 			diff = math.fabs(phi_th - phi)
 			if(diff > max_dev): 
@@ -70,12 +79,27 @@ for ix in range(sizeX):
 				max_diff_ix = ix
 				max_diff_iy = iy
 				max_diff_phy = phi_th
+			#-------gradient---------------------------
+			if(dist > 2*step_total):
+				(gradX,gradY) = gridPhi.calcGradient(x,y)
+				gradX_Th = - charge*(x-x0)/(dist*dist)
+				gradY_Th = - charge*(y-y0)/(dist*dist)
+				grad_diff = 100.0*math.sqrt((gradX_Th-gradX)**2+(gradY_Th-gradY)**2)/math.sqrt(gradX_Th**2+gradY_Th**2)
+				if(grad_diff > max_grad_diff): 
+					max_grad_diff = grad_diff	
+					max_grad_diff_ix = ix
+					max_grad_diff_iy = iy
 
 print "charge position ix =",ind_x
 print "charge position iy =",ind_y
+print "potential at the cahrge position phi =",gridPhi.getValueOnGrid(ind_x,ind_y)
 print "================================"
-print "max deviation =",max_dev
+print "max deviation          =",max_dev
 print "phi theory =",max_diff_phy
 print "max diff position ix = ",max_diff_ix
 print "max diff position iy = ",max_diff_iy
+print "================================"
+print "max gradient deviation [%] =","%5.3f"%max_grad_diff
+print "max grad diff position ix = ",max_grad_diff_ix
+print "max grad diff position iy = ",max_grad_diff_iy
 print "Stop."
