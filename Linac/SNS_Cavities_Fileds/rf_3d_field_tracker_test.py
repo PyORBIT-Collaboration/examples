85020e2ad51b5651466b364a23a872628d55a2a6
Andrei Shishlo <shishlo@hotmail.com>
2013-03-25 21:47:48 +0000

    The RK4 tracker and SuperFish RF Gap field source is not tested completely yet.

--- Linac/SNS_Cavities_Fileds/rf_3d_field_tracker_test.py
+++ Linac/SNS_Cavities_Fileds/rf_3d_field_tracker_test.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
 # This script reads the SuperFish file and creates the SuperFishFieldSource
-#  and the Runge-Kutta 3D tracker. The tracker tracks the bunch of particles
-#  through the RF cavity.
+# and the Runge-Kutta 3D tracker. The tracker tracks the bunch of particles
+# through the RF cavity. The particles that are ahead of after the synch.part.
+# will be decelerated/accelerated.
 #--------------------------------------------------------------------------
-
 import sys
 import math
 
@@ -17,6 +17,7 @@ from linac import SuperFishFieldSource
 
 fReader = SuperFish_3D_RF_FieldReader()
 fReader.readFile("data/scl_medium_beta_rf_cav_field.dat")
+#fReader.readFile("data/scl_high_beta_rf_cav_field.dat")
 (grid2D_Ez,grid2D_Er,grid2D_H) = fReader.makeGrid2DFileds_EzErH()
 
 fieldSource = SuperFishFieldSource()
@@ -25,23 +26,21 @@ fieldSource.setGrid2D_Fields(grid2D_Ez,grid2D_Er,grid2D_H)
 #----------------------------------------------
 # RF field parameters 
 #----------------------------------------------
-rf_freq = 805.0e+6        # in Hz
-zSimmetric = 0            # it is not symmetric
-zOrientation = +1         # the cavity is oriented as in the input file
-amplitude = 1000000.           # the initial amplitude. It means nothing.
-phase = 30.*math.pi/180.  # the initial phase
-fieldCenterPos = (grid2D_Ez.getMinX()+grid2D_Ez.getMaxX())/2.0
-directionZ = +1
-symmetry = 0
+rf_freq = 805.0e+6                # in Hz
+zSimmetric = 0                    # it is not symmetric
+zOrientation = -1                 # the cavity is oriented as in the input file
+amplitude = 20.0e+6               # the initial amplitude. It is just a number.
+phase = (270.-90.)*math.pi/180.  # the initial phase
+time_init = 0.                    # initial time
 
 fieldSource = SuperFishFieldSource()
 fieldSource.setGrid2D_Fields(grid2D_Ez,grid2D_Er,grid2D_H)
 fieldSource.setFrequency(rf_freq)
 fieldSource.setAmplitude(amplitude)
 fieldSource.setPhase(phase)
-fieldSource.setFieldCenterPos(fieldCenterPos)
-fieldSource.setDirectionZ(directionZ)
-fieldSource.setSymmetry(symmetry)
+fieldSource.setDirectionZ(zOrientation)
+fieldSource.setSymmetry(zSimmetric)
+fieldSource.setTimeInit(time_init)
 
 print "frequnecy=",fieldSource.getFrequency()
 print "amplitude=",fieldSource.getAmplitude()
@@ -50,46 +49,59 @@ print "fieldCenterPos = ",fieldSource.getFieldCenterPos()
 print "directionZ=",fieldSource.getDirectionZ()
 print "symmetry = ",fieldSource.getSymmetry()
 print "min max Z =",(grid2D_Ez.getMinX(),grid2D_Ez.getMaxX())
+print "min max R =",(grid2D_Ez.getMinY(),grid2D_Ez.getMaxY())
 print "length =",(grid2D_Ez.getMaxX()-grid2D_Ez.getMinX())
+print "initial time [sec] =",fieldSource.getTimeInit()
+
 #-------Bunch definition ------------------
 b = Bunch()
 print "Part. m=",b.mass()
 print "Part. q=",b.charge()
+syncPart = b.getSyncParticle()
 
-TK = 0.1856           # in [GeV]
+TK = 0.400           # in [GeV]
 E = b.mass() + TK
 P = math.sqrt(E*E - b.mass()*b.mass())
 c_light = 2.99792458e+8
+lmbd = c_light/fieldSource.getFrequency()
 
+syncPart.kinEnergy(TK)
 
 print "TK[GeV] = ",TK
 print "P[GeV/c] = ",P
 
-b.addParticle(0.,0.,0.,0.,0.,0.)
+print "lambda [mm] =",lmbd*1000.
+print "beta*lambda/360 deg [mm/deg] =",lmbd*syncPart.beta()*1000./360.
+
+b.addParticle(0.0,0.0,0.0,0.,0.005,0.)
+b.addParticle(0.0,0.0,0.0,0.,-0.005,0.)
+
 b.compress()
-syncPart = b.getSyncParticle()
-syncPart.kinEnergy(TK)
+
 print "initial syncPart (px,py,pz) =",(syncPart.px(),syncPart.py(),syncPart.pz())
 
 length = grid2D_Ez.getMaxX()-grid2D_Ez.getMinX()
 
 tracker = RungeKuttaTracker(length)
-
-tracker.entrancePlane(0,0,-1.,grid2D_Ez.getMinX())
+#-------------------------------------------------------------------------------
+# for the symmetric fields (if zSimmetric == +1) the grid2D has only z = 0,z_max
+#-------------------------------------------------------------------------------
+if(fieldSource.getSymmetry() == 1):
+	tracker.entrancePlane(0,0,-1.,-grid2D_Ez.getMaxX())
+else:
+	tracker.entrancePlane(0,0,-1.,grid2D_Ez.getMinX())
 tracker.exitPlane(0,0,1.,-grid2D_Ez.getMaxX())
+tracker.spatialEps(0.0000001)
+tracker.stepsNumber(60)
 
 print "Entrance plane (a,b,c,d)=",tracker.entrancePlane()
 print "Exit     plane (a,b,c,d)=",tracker.exitPlane()
 print "Length[m]=",tracker.length()
-
-#b.dumpBunch()
-
-tracker.trackBunch(b,fieldSource)
-
-b.dumpBunch()
-
-
-
+	
+b1 = Bunch()
+b.copyBunchTo(b1)
+tracker.trackBunch(b1,fieldSource)
+b1.dumpBunch()
+	
 print "=========================================="
 print "Done."
-
