ba6e9f479351ffd23fc4abc7f1e4a83eea28cdef
Andrei Shishlo <shishlo@hotmail.com>
2013-06-11 05:09:23 +0000

    The Three Point Rf Gap model tested for TTF calculations.

--- Linac/RF_Gap_Models/rf_gap_three_point_ttf_test.py
+++ Linac/RF_Gap_Models/rf_gap_three_point_ttf_test.py
@@ -1,14 +1,15 @@
 #! /usr/bin/env python
 
 """
-This script is a test for RfGapThreePointTTF gap model. 
+This script is a test for TTF of the RfGapThreePointTTF gap model. 
 This model uses T,T',S,S' transit time factors (TTF) 
 calculated for the second order of polynomial defined by three points 
 of the electric field on the axis Em,E0,Ep for -dz,0.,+dz positions.
 This class does not use the Polynomial class. Instead it uses
-just E(z) = E0*(1+a*z+b*z^2).
+just E(z) = E0*(1+a*z+b*z^2) representation.
+The script will compare TTF from the C++ class with the direct integration
+of the TTF integrals.
 """
-
 import sys
 import math
 import random
@@ -50,70 +51,63 @@ print "Ep=",Ep," E_Func(+dz)= ",FieldFunction(+dz)
 E0L = E0*(2*dz+(2./3.)*b_param*dz*dz*dz)
 
 def T_TTF(kappa, n = 32):
-	f = Function()
-	z_step = 2.0*dz/(n-1)
-	for i in range(n):
-		z = -dz + i*z_step
-		f.add(z,FieldFunction(z)*math.cos(kappa*z))
-	spline = SplineCH()
-	spline.compile(f)	
 	integrator = GaussLegendreIntegrator(n)	
 	integrator.setLimits(-dz,+dz)
-	return integrator.integral(spline)/E0L
+	point_weight_arr = integrator.getPointsAndWeights()
+	res = 0.
+	for (z,w) in point_weight_arr:
+		res += w*FieldFunction(z)*math.cos(kappa*z)
+	#return integrator.integral(spline)/E0L
+	return res/E0L
 
 def Tp_TTF(kappa, n = 32):
-	f = Function()
-	z_step = 2.0*dz/(n-1)
-	for i in range(n):
-		z = -dz + i*z_step
-		f.add(z,-z*FieldFunction(z)*math.sin(kappa*z))
-	spline = SplineCH()
-	spline.compile(f)	
 	integrator = GaussLegendreIntegrator(n)	
 	integrator.setLimits(-dz,+dz)
-	return integrator.integral(spline)/E0L
+	point_weight_arr = integrator.getPointsAndWeights()
+	res = 0.
+	for (z,w) in point_weight_arr:
+		res += -z*w*FieldFunction(z)*math.sin(kappa*z)
+	#return integrator.integral(spline)/E0L
+	return res/E0L	
+	
 
 def S_TTF(kappa, n = 32):
-	f = Function()
-	z_step = 2.0*dz/(n-1)
-	for i in range(n):
-		z = -dz + i*z_step
-		f.add(z,FieldFunction(z)*math.sin(kappa*z))
-	spline = SplineCH()
-	spline.compile(f)	
 	integrator = GaussLegendreIntegrator(n)	
 	integrator.setLimits(-dz,+dz)
-	return integrator.integral(spline)/E0L
+	point_weight_arr = integrator.getPointsAndWeights()
+	res = 0.
+	for (z,w) in point_weight_arr:
+		res += w*FieldFunction(z)*math.sin(kappa*z)
+	#return integrator.integral(spline)/E0L
+	return res/E0L		
 
 def Sp_TTF(kappa, n = 32):
-	f = Function()
-	z_step = 2.0*dz/(n-1)
-	for i in range(n):
-		z = -dz + i*z_step
-		f.add(z,z*FieldFunction(z)*math.cos(kappa*z))
-	spline = SplineCH()
-	spline.compile(f)	
 	integrator = GaussLegendreIntegrator(n)	
 	integrator.setLimits(-dz,+dz)
-	return integrator.integral(spline)/E0L
+	point_weight_arr = integrator.getPointsAndWeights()
+	res = 0.
+	for (z,w) in point_weight_arr:
+		res += w*z*FieldFunction(z)*math.cos(kappa*z)
+	#return integrator.integral(spline)/E0L
+	return res/E0L		
+	
 
 print "==============================================="
-res_T = T_TTF(kappa,256)
+res_T = T_TTF(kappa,3)
 res_T_test = rf_gap.getT_TTF(dz,a_param,b_param,kappa)
 print "T = %16.9g "%res_T," cpp_model= %16.9g"%res_T_test
 
-res_Tp = Tp_TTF(kappa,256)
+res_Tp = Tp_TTF(kappa,3)
 res_Tp_test = rf_gap.getTp_TTF(dz,a_param,b_param,kappa)
 print "Tp= %16.9g "%res_Tp," cpp_model= %16.9g"%res_Tp_test
 
-res_S = S_TTF(kappa,256)
+res_S = S_TTF(kappa,3)
 res_S_test = rf_gap.getS_TTF(dz,a_param,b_param,kappa)
 print "S = %16.9g "%res_S," cpp_model= %16.9g"%res_S_test
 
-res_Sp = Sp_TTF(kappa,256)
+res_Sp = Sp_TTF(kappa,3)
 res_Sp_test = rf_gap.getSp_TTF(dz,a_param,b_param,kappa)
 print "Sp= %16.9g "%res_Sp," cpp_model= %16.9g"%res_Sp_test
 
 print "=========================================="
 print "Stop."
-
