a808edbf158f2adac89bf7fa7163915ebc53718d
reatmj <reatmj@gmail.com>
2013-06-11 16:53:39 +0000

    changed files to make the project compile

--- FieldTracker/fieldtracktest.py
+++ FieldTracker/fieldtracktest.py
@@ -27,36 +27,36 @@ b.getSyncParticle().kinEnergy(energy)
 
 myparser = Field_Parser3D()
 print "FILE NEEDS TO NOT CONTAIN A HEADER OR BLANK LINES"
-data = myparser.parse("BIGDATA~", -20.0,20.0,-12.0, 12.0 ,0.0, 250.0 , 0.5 , 0.5 , 0.5)
-
-# for i in range(10):
-#   print "At Coordinates " , i , ", " , i , ", " , i 
-#   print BXGrid.getValueOnGrid(i,i,i), BYGrid.getValueOnGrid(i,i,i), BZGrid.getValueOnGrid(i,i,i)
-
-## Variables to test BGrid3D
-BXGrid = data[0]
-BYGrid = data[1]
-BZGrid = data[2]
-fieldgrid3DMag = data[3]
-XGrid = data[4]
-YGrid = data[5]
-ZGrid = data[6]
-
-print "X, Y, Z Grid Size: ", len(XGrid), len(YGrid), len(ZGrid)
-
-nXGrid = len(XGrid)
-nYGrid = len(YGrid)
-nZGrid = len(ZGrid)
-
-xField3D = 0.2
-yField3D = 0.2
-zField3D = 0.2
-
-BxField3D = 0.0
-ByField3D = 0.0
-BzField3D = 0.0
-
-zsymmetry = 1
+# data = myparser.parse("BIGDATA~", -20.0,20.0,-12.0, 12.0 ,0.0, 250.0 , 0.5 , 0.5 , 0.5)
+# 
+# # for i in range(10):
+# #   print "At Coordinates " , i , ", " , i , ", " , i 
+# #   print BXGrid.getValueOnGrid(i,i,i), BYGrid.getValueOnGrid(i,i,i), BZGrid.getValueOnGrid(i,i,i)
+# 
+# ## Variables to test BGrid3D
+# BXGrid = data[0]
+# BYGrid = data[1]
+# BZGrid = data[2]
+# fieldgrid3DMag = data[3]
+# XGrid = data[4]
+# YGrid = data[5]
+# ZGrid = data[6]
+# 
+# print "X, Y, Z Grid Size: ", len(XGrid), len(YGrid), len(ZGrid)
+# 
+# nXGrid = len(XGrid)
+# nYGrid = len(YGrid)
+# nZGrid = len(ZGrid)
+# 
+# xField3D = 0.2
+# yField3D = 0.2
+# zField3D = 0.2
+# 
+# BxField3D = 0.0
+# ByField3D = 0.0
+# BzField3D = 0.0
+# 
+# zsymmetry = 1
 
 
 
