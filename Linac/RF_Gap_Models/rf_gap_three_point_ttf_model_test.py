8255e1d2284caf1df39d08cf546321ec88177ae1
Andrei Shishlo <shishlo@hotmail.com>
2013-06-17 02:20:33 +0000

    The Three Point TTF RF gap model tested.

--- Linac/RF_Gap_Models/rf_gap_three_point_ttf_model_test.py
+++ Linac/RF_Gap_Models/rf_gap_three_point_ttf_model_test.py
@@ -19,32 +19,133 @@ from bunch import Bunch
 from linac import BaseRfGap, MatrixRfGap, RfGapTTF, RfGapThreePointTTF 
 from orbit.sns_linac import Drift
 
+# The classes for the field on axis of the cavity
+from orbit.sns_linac.rf_field_readers import SuperFish_3D_RF_FieldReader, RF_AxisFieldAnalysis
+
+def makePhaseNear(phase, phase0):                                                                                                                                                          
+	""" It will add or substruct any amount of 360. from phase to get close to phase0 """                                                                                              
+	n = int(phase0/360.)                                                                                                                                                               
+	phase = phase%360.                                                                                                                                                                 
+	min_x = 1.0e+38                                                                                                                                                                    
+	n_min = 0                                                                                                                                                                          
+	for i0 in range(5):                                                                                                                                                                
+		i = i0 - 3                                                                                                                                                                 
+		d = math.fabs(phase + 360.*(i+n) - phase0)                                                                                                                                 
+		if(d < min_x):                                                                                                                                                             
+			n_min = i                                                                                                                                                          
+			min_x = d                                                                                                                                                          
+	return (phase + 360.*(n_min+n)) 
+
 rf_gap = RfGapThreePointTTF()
 
 rf_frequency = 400.0*1.0e+6   # in Hz
-dz = 0.01                     # in m
-Em = 12.0e+6                  # in V/m
-E0 = 13.2e+6                  # in V/m
-Ep = 14.0e+6                  # in V/m
 
-#kappa = 2*PI*frequency/(c*beta)
+#read the RF cavity field
+fReader = SuperFish_3D_RF_FieldReader()
+fReader.readFile("../SNS_Cavities_Fields/data/scl_medium_beta_rf_cav_field.dat")
+
+#This particular cavity is not symmetric
+zSimmetric = 0
+spline = fReader.getAxisEz(zSimmetric)
+
+rf_analysis = RF_AxisFieldAnalysis(spline)
+root_pos_arr = rf_analysis.rootAnalysis()
+center_pos_arr = rf_analysis.gapCentersAnalysis()
+print "roots=",root_pos_arr
+print "centers=",center_pos_arr
+
+spline = rf_analysis.getNormilizedSpline()
+
+n_gap_steps = 10
+z_min = spline.x(0)
+z_max = spline.x(spline.getSize() - 1)
+step_size = (z_max - z_min)/(n_gap_steps - 1)
+
+dz = step_size/2.0
+drift = Drift()
+drift.setLength(dz)
+
+def trackBunch(b,cavity_amp,phase_dgr):
+	phase = phase_dgr*math.pi/180.	
+	time_init = b.getSyncParticle().time()
+	for i in range(n_gap_steps-1):
+		zm = z_min + i*step_size
+		z0 = zm + dz
+		zp = z0 + dz
+		Em = cavity_amp*spline.getY(zm)
+		E0 = cavity_amp*spline.getY(z0)
+		Ep = cavity_amp*spline.getY(zp)
+		drift.setLength(dz)
+		drift.trackBunch(b)
+		time_gap = b.getSyncParticle().time()
+		delta_phase = 2*math.pi*(time_gap - time_init)*rf_frequency
+		rf_gap.trackBunch(b,dz,Em,E0,Ep,rf_frequency,phase+delta_phase)
+		#print "====debug Python level========="
+		#b.dumpBunch()
+		drift.setLength(dz)
+		drift.trackBunch(b)
+		#b.dumpBunch()
+	return b
+
+#---------------------------------------
+#---- let's make bunch ---------
+#---------------------------------------
+b = Bunch()
+print "Part. m=",b.mass()
+print "Part. q=",b.charge()
+TK = 0.1856                    # in [GeV]
+syncPart = b.getSyncParticle()
+syncPart.kinEnergy(TK)
+beta = syncPart.beta()
+gamma = syncPart.gamma() 
+mass = syncPart.mass()
+
 c_light = 2.99792458e+8
-beta = 0.5
-kappa = 2*math.pi*rf_frequency/(c_light*beta)
-print "kappa =",kappa
+lmbd = c_light/rf_frequency
 
-a_param = (Ep-Em)/(2*E0*dz)
-b_param = (Ep+Em-2*E0)/(2*E0*dz*dz)
+#---cavity field
+E0 =  20.0e+6       # average field in V/m
 
-def FieldFunction(z):
-	return E0*(1. + a_param*z + b_param*z*z)
+b.addParticle(0.001,0.0,0.000,0.,0.,0.)
 
-print "Em=",Em," E_Func(-dz)= ",FieldFunction(-dz)
-print "E0=",E0," E_Func(  0)= ",FieldFunction(0.)
-print "Ep=",Ep," E_Func(+dz)= ",FieldFunction(+dz)
+eKin_min = 1.0e+40
+eKin_max =-1.0e+40
 
-E0L = E0*(2*dz+(2./3.)*b_param*dz*dz*dz)
+k_r_min = 1.0e+40
+k_r_max =-1.0e+40
 
+phase_start = 0.
+phase_step = 1.0
+n_steps = int(360./phase_step) + 1
+phase_maxE = 0.
+print "#  phase[deg]        Ek[MeV]   k_x " 
+for i in range(n_steps):
+	phase = phase_start + i*phase_step
+	b1 = Bunch()
+	b.copyBunchTo(b1)
+	b1 = trackBunch(b1,E0,phase)
+	k_r = b1.xp(0)/b1.x(0)
+	eKin_out = b1.getSyncParticle().kinEnergy()
+	if(eKin_min > eKin_out):
+		eKin_min = eKin_out
+	if(eKin_max < eKin_out):
+		eKin_max = eKin_out
+		phase_maxE = phase
+	if(k_r_min > k_r):
+		k_r_min = k_r
+	if(k_r_max < k_r):
+		k_r_max = k_r
+	print " %3d "%i," %5.1f "%phase," %12.6f "%(eKin_out*1000)," %12.6f "%k_r
+
+E0TL = 	(eKin_max - eKin_min)/2.0
+beta = syncPart.beta()
+gamma = syncPart.gamma() 
+mass = syncPart.mass()
+print "maximal Ekin at RF phase=",makePhaseNear(phase_maxE,0.)
+print " E0TL [MeV] = %12.6f "%(E0TL*1000)
+k_r_theory = math.pi*E0TL/(mass*gamma**3*beta**3*lmbd)
+print "focusing coef. theory     xp/r = %12.5g "%k_r_theory 
+print "focusing coef. RF TTF Gap xp/r = %12.5g "%((k_r_max - k_r_min)/2.0)
 
 print "=========================================="
 print "Stop."
