afb57c14d7ec61b29e9b8905f41ee09a7a580f90
reatmj <reatmj@gmail.com>
2013-08-07 13:59:26 +0000

    Updated examples

--- FieldTracker/fieldtracktest.py
+++ FieldTracker/fieldtracktest.py
@@ -25,61 +25,79 @@ b.getSyncParticle().kinEnergy(energy)
 # b.readBunch("Bunches/controlbunch_600.dat", 1)
 
 
-#=====track bunch through Foil============
-# 
-# myparser = Field_Parser3D()
 
-# data = myparser.parse("BIGDATA~", -20.0,20.0,-12.0, 12.0 ,0.0, 250.0 , 0.5 , 0.5 , 0.5)
-# 
-# # for i in range(10):
-# #   print "At Coordinates " , i , ", " , i , ", " , i 
-# #   print BXGrid.getValueOnGrid(i,i,i), BYGrid.getValueOnGrid(i,i,i), BZGrid.getValueOnGrid(i,i,i)
-# 
-# ## Variables to test BGrid3D
-# BXGrid = data[0]
-# BYGrid = data[1]
-# BZGrid = data[2]
-# fieldgrid3DMag = data[3]
-# XGrid = data[4]
-# YGrid = data[5]
-# ZGrid = data[6]
-# 
-# print "X, Y, Z Grid Size: ", len(XGrid), len(YGrid), len(ZGrid)
-# 
-# nXGrid = len(XGrid)
-# nYGrid = len(YGrid)
-# nZGrid = len(ZGrid)
-# 
-# xField3D = 0.2
-# yField3D = 0.2
-# zField3D = 0.2
-# 
-# BxField3D = 0.0
-# ByField3D = 0.0
-# BzField3D = 0.0.
+#------------------------------------------
+#Initial Variables
+#------------------------------------------
+LA11RB  = 0.940222788932
+LC11F1  = 0.307193
+LC11C12 = 1.8144
+LA12RB  = 0.908319544743
+XA11M   = 92.55
+YA11M   = 23.0
+XFOIL1  = 150.4732
+YFOIL1  = 46.0
+XPFOIL1  = -0.65387068
+
+XMAG = XA11M
+YMAG = YA11M
+
+XSTART = XFOIL1
+YSTART = YFOIL1
+XPSTART = XPFOIL1
+YPSTART = 0.0
+
+XREFI = XSTART - XMAG
+YREFI = YSTART - YMAG
+XREFITOT = 0.0
+YREFITOT = 0.0
+
+EULERA = 0.0
+EULERB = -0.65387059
+EULERG = 0.0
+EULERATOT = 0.0
+EULERBTOT = 0.0
+EULERGTOT = 0.0
+
+ZSTARTTOT = -2.5
+ZSTART = LC11F1
+ZTOT = 2.5
+ZINT = LC11C12 / 2.0
+LTOT = ZTOT - ZSTARTTOT
+LINT = ZINT - ZSTART
+LINTTOT = ZTOT - ZSTART
+
+XPARSEMIN = -21.0
+XPARSEMAX =  21.0
+YPARSEMIN = -13.0
+YPARSEMAX =  13.0
+
+ZPARSEMIN =  100.0 * ZSTART - 1.0
+ZPARSEMAX =  100.0 * ZINT + 1.0
+
+SKIPX = 1 
+SKIPY = 1
+SKIPZ = 1
+
+#=====track bunch through Foil============
  
 zsymmetry = 1
+printPaths = 1;
 
-mytracker = FieldTracker(0)
+
+mytracker = FieldTracker( 10.190, 10.758, 0.047, 0.056, -0.001, -0.012,
+          LINT,  
+          ZSTART, ZINT, 0.0001, 2, 1.e-06,
+          XREFI, YREFI, EULERA, EULERB, EULERG,b, "testfile.data")
+
+mytracker.getBunchPath(1);
+
 mytracker.trackBunch(b)
-
-
-
-
-# FieldArr = mytracker.BGrid3D(xField3D,yField3D,zField3D,
-# XGrid,YGrid,ZGrid,
-# nXGrid, nYGrid, nZGrid,
-# BxField3D, ByField3D, BzField3D,
-# BXGrid,  BYGrid, BZGrid,
-# zsymmetry)
-
-
-
 
 
 b.dumpBunch("final.dat")
 
-print "Stop."
+print "\nStop."
 
 
 
