03b4404331c698f41874fcbf17cea612bf111eed
Andrei Shishlo <shishlo@hotmail.com>
2015-02-23 21:22:43 +0000

    The XML data adaptor now has a "reading from the file" method

--- Utils/XML/xml_data_adaptor_test.py
+++ Utils/XML/xml_data_adaptor_test.py
@@ -16,7 +16,23 @@ chld1 = xml_data_adaptor.createChild("child1")
 chld2 = chld1.createChild("child2")
 chld2.setValue("arr_double1",[-1.,-2.,-3.,-4.])
 chld2.setValue("val",0.123456789e-17)
+xml_text = xml_data_adaptor.makeXmlText()
+print xml_text
 xml_data_adaptor.writeToFile("test.xml")
+print "==================writing to a file is done============================"
+print "======================================================================="
+
+xml_data_adaptor_new = XmlDataAdaptor.adaptorForFile("test.xml")
+xml_text = xml_data_adaptor_new.makeXmlText()
+print "====================After reading the file============================="
+print xml_text
+print "======================================================================="
+child1 = xml_data_adaptor_new.childAdaptors("child1")[0]
+child2 = child1.childAdaptors("child2")[0]
+print " child2 int arr =",child2.intArrayValue("arr_double1")
+print " child2 double arr =",child2.doubleArrayValue("arr_double1")
+print " child2 double val =",child2.doubleArrayValue("val")
+print " child2 double val =",child2.doubleValue("val")
 
 print "Stop."
 
