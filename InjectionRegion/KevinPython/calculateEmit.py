40fdc02ed205015f2b4eb1839385c48554488bae
khilde <hildebrandkr@ornl.gov>
2021-06-08 09:30:53 -0400

    added in pencil beam stripping

---
+++ InjectionRegion/KevinPython/calculateEmit.py
@@ -0,0 +1,155 @@
+
+from orbit.teapot.teapot import NodeTEAPOT
+import math
+from bunch import BunchTwissAnalysis
+class Calc_Emit(NodeTEAPOT):
+    """
+    Generates the Histograms for distribution in x,y, and x=y directions.
+    The z direction from the original WS_Node is repurposed for the diagonal x=y.
+    It is not parallel !!!!
+    """
+    def __init__(self,name = "CalcEmit",toFile=False,fileName="output.txt"):
+        NodeTEAPOT.__init__(self,name)
+        self.toFile=toFile
+        self.fileName=fileName
+
+    def track(self, paramsDict):
+        """
+        The WS_AccNode class implementation of the AccNode class track(probe) method.
+        """
+        debug=False
+        bunch = paramsDict["bunch"]
+        gamma = bunch.getSyncParticle().gamma()
+        beta = bunch.getSyncParticle().beta()
+        
+        tot_x=0
+        tot_px=0
+        tot_y=0
+        tot_py=0
+        
+        tot_x2=0
+        tot_px2=0
+        tot_x_px=0
+        
+        tot_y2=0
+        tot_py2=0
+        tot_y_py=0       
+        if bunch.getSize() >1:
+		for i in range(bunch.getSize()):   
+			tot_x=tot_x+bunch.x(i)*1000.
+			tot_x2=tot_x2+bunch.x(i)*1000.*bunch.x(i)*1000.
+			tot_px=tot_px+bunch.px(i)*1000.
+			tot_px2=tot_px2+bunch.px(i)*1000.*bunch.px(i)*1000.
+			tot_y=tot_y+bunch.y(i)*1000.
+			tot_y2=tot_y2+bunch.y(i)*1000.*bunch.y(i)*1000.
+			tot_py=tot_py+bunch.py(i)*1000.
+			tot_py2=tot_py2+bunch.py(i)*1000.*bunch.py(i)*1000.
+			tot_x_px=tot_x_px+bunch.x(i)*1000.*bunch.px(i)*1000.
+			tot_y_py=tot_y_py+bunch.y(i)*1000.*bunch.py(i)*1000.       	
+		avg_x=tot_x/bunch.getSize()
+		avg_px=tot_px/bunch.getSize()
+		avg_y=tot_y/bunch.getSize()
+		avg_py=tot_py/bunch.getSize() 
+		
+		avg_x2=tot_x2/bunch.getSize()
+		avg_px2=tot_px2/bunch.getSize()
+		avg_x_px=tot_x_px/bunch.getSize()
+		
+		avg_y2=tot_y2/bunch.getSize()
+		avg_py2=tot_py2/bunch.getSize()
+		avg_y_py=tot_y_py/bunch.getSize()  
+		
+		var_x=avg_x2-math.pow(avg_x,2)
+		var_px=avg_px2-math.pow(avg_px,2)
+		var_x_px=avg_x_px-avg_x*avg_px
+		
+		var_y=avg_y2-math.pow(avg_y,2)
+		var_py=avg_py2-math.pow(avg_py,2)
+		var_y_py=avg_y_py-avg_y*avg_py        
+		if debug==True:
+			print "(tot_x2) = %f"%(tot_x2)
+			print "(tot_px2) = %f"%(tot_px2)
+			print "(tot_x_px) = %f"%(tot_x_px)
+			print "(avg_x2) = %f"%(avg_x2)
+			print "(avg_px2) = %f"%(avg_px2)
+			print "(avg_x_px) = %f"%(avg_x_px)        	
+			print "(emit x)^2 = %f"%(avg_x2*avg_px2-avg_x_px*avg_x_px)
+			print "(tot_y2) = %f"%(tot_y2)
+			print "(tot_py2) = %f"%(tot_py2)
+			print "(tot_y_py) = %f"%(tot_y_py)
+			print "(avg_y2) = %f"%(avg_y2)
+			print "(avg_py2) = %f"%(avg_py2)
+			print "(avg_y_py) = %f"%(avg_y_py)        	
+			print "(emit y)^2 = %f"%(avg_y2*avg_py2-avg_y_py*avg_y_py)    
+			print "(var_x) = %f"%(var_x)
+			print "(var_px) = %f"%(var_px)
+			print "(var_x_px) = %f"%(var_x_px)
+			print "(var_y) = %f"%(var_y)
+			print "(var_py) = %f"%(var_py)
+			print "(var_y_py) = %f"%(var_y_py)
+			print "(var_x*var_px-var_x_px*var_x_px) = %f"%(var_x*var_px-var_x_px*var_x_px)   
+		emit_x=math.sqrt(var_x*var_px-var_x_px*var_x_px)
+		emit_y=math.sqrt(var_y*var_py-var_y_py*var_y_py)
+		
+		twiss_analysis = BunchTwissAnalysis()        
+		node = paramsDict["node"]
+		bunch = paramsDict["bunch"]
+		pos = paramsDict["path_length"]
+	
+		gamma = bunch.getSyncParticle().gamma()
+		beta = bunch.getSyncParticle().beta()
+		twiss_analysis.analyzeBunch(bunch)
+		x_rms = math.sqrt(twiss_analysis.getTwiss(0)[1]*twiss_analysis.getTwiss(0)[3])*1000.
+		y_rms = math.sqrt(twiss_analysis.getTwiss(1)[1]*twiss_analysis.getTwiss(1)[3])*1000.
+		z_rms = math.sqrt(twiss_analysis.getTwiss(2)[1]*twiss_analysis.getTwiss(2)[3])*1000.
+		xp_rms= math.sqrt(twiss_analysis.getTwiss(0)[2]*twiss_analysis.getTwiss(0)[3])*1000.
+		yp_rms= math.sqrt(twiss_analysis.getTwiss(1)[2]*twiss_analysis.getTwiss(1)[3])*1000.
+		nParts = bunch.getSizeGlobal()
+		(alphaX,betaX,emittX) = (twiss_analysis.getTwiss(0)[0],twiss_analysis.getTwiss(0)[1],twiss_analysis.getTwiss(0)[3]*1.0e+6)
+		(alphaY,betaY,emittY) = (twiss_analysis.getTwiss(1)[0],twiss_analysis.getTwiss(1)[1],twiss_analysis.getTwiss(1)[3]*1.0e+6)
+		norm_emittX = emittX*gamma*beta
+		norm_emittY = emittY*gamma*beta
+		#---- phi_de_emittZ will be in [pi*deg*MeV]
+		eKin = bunch.getSyncParticle().kinEnergy()*1.0e+3
+	
+		s_prt = " %35s  %4.5f "%(node.getName(),pos)
+		s_prt += "  %5.3f  %5.3f  "%(x_rms,y_rms)
+		s_prt += "  %10.6f   %8d "%(eKin,nParts)
+		print s_prt	        
+		if (self.toFile==False):
+		 
+			print " (emit x, emit y)= (%f,%f) " %(emit_x,emit_y)
+		else:
+		    fileOut=open(self.fileName,'a')
+		    #print self.getName()
+		    s = " %35s  %4.5f \n"%(node.getName(),pos)
+		    s += "   %6.4f  %6.4f  %6.4f  %6.4f   \n"%(alphaX,betaX,emittX,norm_emittX)
+		    s += "   %6.4f  %6.4f  %6.4f  %6.4f   \n"%(alphaY,betaY,emittY,norm_emittY)
+		    s += "   %5.3f  %5.3f \n"%(x_rms,y_rms)
+		    s += "   %5.3f  %5.3f \n"%(xp_rms,yp_rms)
+		    s += "  %10.6f   %8d "%(eKin,nParts)
+		    fileOut.write(s +"\n")
+		    fileOut.flush()            
+		    fileOut.write(" (x rms, y rms)= (%f,%f) \n" %(math.sqrt(var_x),math.sqrt(var_y)))
+		    fileOut.write(" (xp rms, yp rms)= (%f,%f) \n" %(math.sqrt(var_px),math.sqrt(var_py)))
+		    fileOut.write(" (x avg, y avy)= (%f,%f) \n" %(avg_x,avg_y))
+		    fileOut.write(" (xp avg, yp avg)= (%f,%f) \n" %(avg_px,avg_py))		    
+		    fileOut.write(" (emit x, emit y)= (%f,%f) \n" %(emit_x,emit_y))
+		    fileOut.write(" (normal emit x, normal emit y)= (%f,%f) " %(emit_x*beta*gamma,emit_y*beta*gamma))
+		    fileOut.close()
+	elif bunch.getSize() ==1:
+	    fileOut=open(self.fileName,'a')
+	    #print self.getName()
+	    node = paramsDict["node"]
+	    pos = paramsDict["path_length"]
+	    s = " %35s  %4.5f \n"%(node.getName(),pos)
+	    fileOut.write(s +"\n")
+	    fileOut.flush()       
+	    fileOut.write("pencilbeam \n")
+	    fileOut.write(" (x rms, y rms)= (%f,%f) \n" %(0,0))
+	    fileOut.write(" (xp rms, yp rms)= (%f,%f) \n" %(0,0))
+	    fileOut.write(" (x avg, y avy)= (%f,%f) \n" %(bunch.x(0),bunch.y(0)))
+	    fileOut.write(" (xp avg, yp avg)= (%f,%f) \n" %(bunch.px(0),bunch.py(0)))		    
+	    fileOut.write(" (emit x, emit y)= (%f,%f) \n" %(0,0))
+	    fileOut.write(" (normal emit x, normal emit y)= (%f,%f) " %(0,0))
+	    fileOut.close()		
---
+++ InjectionRegion/injectionRegionOnly_OptimizerStripper.py
@@ -0,0 +1,375 @@
+##############################################################
+# This script reads the input MAD file with lattice information,
+# creates the TEAPOT lattice, and modifies this lattice by inserting 
+# injection nodes
+##############################################################
+
+import math
+import sys
+
+from orbit.teapot import teapot
+from orbit.teapot import TEAPOT_Lattice
+from orbit.teapot import DriftTEAPOT
+from orbit.lattice import AccLattice, AccNode, AccActionsContainer
+from bunch import Bunch
+from orbit.utils.orbit_mpi_utils import bunch_orbit_to_pyorbit, bunch_pyorbit_to_orbit
+from orbit.injection import TeapotInjectionNode
+from orbit.injection import addTeapotInjectionNode
+from orbit.injection import InjectParts
+from orbit.injection import JohoTransverse, JohoLongitudinal, SNSESpreadDist
+from orbit.kickernodes import XKicker, YKicker
+from orbit.kickernodes import rootTWaveform, flatTopWaveform
+from orbit.kickernodes import TeapotXKickerNode, TeapotYKickerNode,addTeapotKickerNode
+from orbit.foils import TeapotFoilNode, addTeapotFoilNode
+from foil import Foil
+from orbit.collimation import TeapotCollimatorNode, addTeapotCollimatorNode
+from orbit.space_charge.sc2p5d import scAccNodes, scLatticeModifications
+from spacecharge import SpaceChargeCalc2p5D, Boundary2D
+from spacecharge import LSpaceChargeCalc
+from orbit.space_charge.sc1d import addLongitudinalSpaceChargeNode, SC1D_AccNode
+from orbit.rf_cavities import RFNode, RFLatticeModifications
+from spacecharge import Boundary2D
+
+from KevinPython import notRandom
+from orbit.teapot import GeneralDipole
+from orbit.teapot import YDipole
+from orbit.teapot import XDipole
+from orbit.teapot import GeneralDipoleNoStrip
+from KevinPython.printNode import Print_Node
+import argparse
+
+from orbit.utils.fitting import Solver
+from orbit.utils.fitting import Scorer
+from orbit.utils.fitting import SolveStopperFactory
+from orbit.utils.fitting import VariableProxy
+from orbit.utils.fitting import TrialPoint
+
+from orbit.utils.fitting import SimplexSearchAlgorithm
+from orbit_utils import Function
+from KevinPython.function_stripping import probabilityStripping
+
+from orbit.bunch_generators import WaterBagDist3D, GaussDist3D, KVDist3D
+from sns_linac_bunch_generator import SNS_Linac_BunchGenerator
+
+class MyScorer(Scorer):
+	""" The implementation of the abstract Score class """
+	def __init__(self,teapot_latt):
+		Scorer.__init__(self)
+		self.teapot_latt=teapot_latt
+		self.xTarget=0
+		self.pxTarget=0
+		self.yTarget=0
+		self.pyTarget=0
+		self.zTarget=0
+		self.dETarget=0
+		
+		self.xInit=0
+		self.pxInit=0
+		self.yInit=0
+		self.pyInit=0
+		self.zInit=0
+		self.dEInit=0	
+		
+		#=====Main bunch parameters============
+		intensity = 7.8e13
+		#turns = 1000.0
+		self.turns=1
+		turns = self.turns
+		#macrosperturn = 260
+		macrosperturn = 1
+		macrosize = intensity/turns/macrosperturn
+		
+		self.b = Bunch()
+		self.b.mass(0.93827231)
+		self.b.macroSize(macrosize)
+		#energy = 1.0 #Gev
+		energy = 1.3 #Gev
+		self.b.getSyncParticle().kinEnergy(energy)
+		self.b.addParticle(0,0,0,0,0,0)
+		
+		self.paramsDict = {}
+		self.paramsDict["bunch"]= self.b
+		self.chicaneNodes=[29,31,34,36]
+		self.chicaneNodeStrength=[-0.041456,0.052434,0.0298523,-0.0398609]
+	#currently assumes bunch has just one macroparticle
+	def resetBunch(self):
+		self.b.x(0,self.xInit)
+		self.b.px(0,self.pxInit)
+		self.b.y(0,self.yInit)
+		self.b.py(0,self.pyInit)
+		self.b.z(0,self.zInit)
+		self.b.dE(0,self.dEInit)
+
+	def setScaleChicane(self,chicaneToScale,scale):
+		nodeIndex=self.chicaneNodes[chicaneToScale]
+		chicaneNode=self.teapot_latt.getNodes()[nodeIndex]
+		chicaneNode.setParam("kx", self.chicaneNodeStrength[chicaneToScale]*scale)
+	def getScaleChicane(self,chicaneToScale):
+		nodeIndex=self.chicaneNodes[chicaneToScale]
+		chicaneNode=self.teapot_latt.getNodes()[nodeIndex]
+		return chicaneNode.getParam("kx")
+	#used to change the location of the chicane node if different from original location
+	def changeChicaneNode(self, nodeToChange, newNodeLocation):
+		self.chicaneNodes[nodeToChange]=newNodeLocation
+	#changes the starting strength of the chicane
+	def setStartStrengthChicane(self,chicaneToChange,strength):
+		self.chicaneNodeStrength[chicaneToChange]=strength
+	#gets the starting strength of the chicane
+	def getStartStrengthChicane(self,chicaneToChange):
+		return self.chicaneNodeStrength[chicaneToChange]
+	#initialize the chicanes in the teapot lattice.
+	def initChicanes(self):
+		chicanewave = flatTopWaveform(1.0)
+		nodes = self.teapot_latt.getNodes()
+		for i in range(4):
+			nodes[self.chicaneNodes[i]].setWaveform(chicanewave)
+			nodes[self.chicaneNodes[i]].setParam("kx",self.chicaneNodeStrength[i])
+		pass		
+	def getScore(self,trialPoint):
+		self.resetBunch()
+		x0 = trialPoint.getVariableProxyArr()[0].getValue()
+		x1 = trialPoint.getVariableProxyArr()[1].getValue()
+		x2 = trialPoint.getVariableProxyArr()[2].getValue()
+		x3 = trialPoint.getVariableProxyArr()[3].getValue()
+		self.setScaleChicane(0,-x0)
+		self.setScaleChicane(1,-x1)
+		self.setScaleChicane(2,-x2)
+		self.setScaleChicane(3,-x3)
+
+		for i in range(self.turns):
+			self.teapot_latt.trackBunch(self.b, self.paramsDict)
+			
+		#score = (b.x(0)-self.xTarget)**2 + (b.px(0)-self.pxTarget)**2 + (b.y(0)-self.yTarget)**2+(b.py(0)-self.pyTarget)**2+(b.z(0)-self.zTarget)**2+(b.pz(0)-self.dETarget)**2
+		score = (self.b.x(0)-self.xTarget)**2 + (self.b.px(0)-self.pxTarget)**2 + (self.b.y(0)-self.yTarget)**2+(self.b.py(0)-self.pyTarget)**2
+
+		return score	
+		
+print "Start."
+parser = argparse.ArgumentParser(description="%prog [options]", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("--doDipoleKickers",type=bool, dest='doDipoleKickers', default=True, help="print node list")
+args = parser.parse_args()
+#=====Make a Teapot style lattice======
+theEffLength=0.03*2
+#theEffLength=0.01
+fieldStrength=1.3
+fieldStrengthMin=.2
+cutLength=0.03
+A1=2.47e-6
+A2=4.49e9
+
+#=====Main bunch parameters============
+intensity = 7.8e13
+#turns = 1000.0
+turns=1
+#macrosperturn = 260
+macrosperturn = 1
+macrosize = intensity/turns/macrosperturn
+bunch_in = Bunch()
+mass =  0.93827231 #0.939294    # in [GeV]
+bunch_in.mass(mass) #mass
+bunch_in.macroSize(macrosize)
+e_kin_ini = 1.3
+energy = e_kin_ini # 1.0 #Gev
+bunch_in.getSyncParticle().kinEnergy(energy)
+bunch_in.charge(+1)
+
+sp = bunch_in.getSyncParticle()
+beta= sp.beta()
+gamma=sp.gamma()
+
+print "beta= %f"%beta
+print "gamma= %f"%gamma
+
+def constantField(x):
+	return fieldStrength
+def pieceWiseField(x):
+	if x<=0.0:
+		return 0
+	elif x<cutLength :
+		return fieldStrength/cutLength*x
+	elif x>=cutLength:
+		return fieldStrength
+	pass
+def pieceWiseField2(x):
+	if x<cutLength :
+		return (fieldStrength-fieldStrengthMin)/cutLength*x+fieldStrengthMin
+	elif x>=cutLength:
+		return fieldStrength
+	pass
+magneticField= Function()
+n=1000
+maxValue=theEffLength
+step=maxValue/n
+
+for i in range(n):
+	x = step*i;
+	#y = constantField(x)
+	y = pieceWiseField2(x)
+	magneticField.add(x,y)
+	
+theStrippingFunctions=probabilityStripping(magneticField,n,maxValue,gamma,beta)
+theStrippingFunctions.computeFunctions()
+accumlatedSum=theStrippingFunctions.getaccumlatedSum()
+CDF=theStrippingFunctions.getCDF()
+deltaxp_rigidity=theStrippingFunctions.getdeltaxp_rigidity()
+deltax_rigidity=theStrippingFunctions.getdeltax_rigidity()
+deltaxp_m_rigidity=theStrippingFunctions.getdeltaxp_m_rigidity()
+deltax_m_rigidity=theStrippingFunctions.getdeltax_m_rigidity()
+InverseFunction=theStrippingFunctions.getInverseFunction()
+
+nPartsChicane=100
+for currentPart in range(nPartsChicane+1):
+	teapot_latt = teapot.TEAPOT_Ring()
+	teapot_latt.readMAD("MAD_Injection_Region_Lattice/InjectionRegionOnly_Chicane_Replaced_With_Kickers.LAT","RING")
+	#print "Lattice=",teapot_latt.getName()," length [m] =",teapot_latt.getLength()," nodes=",len(teapot_latt.getNodes())
+	nodes2 = teapot_latt.getNodes()
+	#numberOfCustomDipoles=2
+	numberOfParts_DH_A11=nPartsChicane
+	numberOfParts_DH_A12=4
+	counterDH_A11=0
+	counterDH_A12=0
+	if args.doDipoleKickers:
+		for node in nodes2:
+			if (node.getName().strip()=="DH_A11"):
+				#pass
+				node.setnParts(numberOfParts_DH_A11)
+				print "total length= ",node.getLength()
+				#print "segment length= ",node.getLength(3)
+				#myDipole_DH_A11=YDipole("Dipole_DH_A11")
+				myDipole_DH_A11=GeneralDipoleNoStrip("Dipole_DH_A11")
+				myDipole_DH_A11.setFunctionXPRigidity(deltaxp_rigidity)
+				myDipole_DH_A11.setFunctionXRigidity(deltax_rigidity)
+				myDipole_DH_A11.setFieldDirection(math.pi/2)
+				myDipole_DH_A11.setEffLength(theEffLength)
+				if currentPart is not nPartsChicane:
+					node.addChildNode(myDipole_DH_A11,AccNode.BODY,currentPart)
+				else:
+					pass
+					#node.addChildNode(myEmitNode_DH11_3pre,AccNode.EXIT,currentPart-1)
+					#node.addChildNode(myPrintNode_DH11_3pre,AccNode.EXIT,currentPart-1)
+					#node.addChildNode(myDipole_DH_A11,AccNode.EXIT,currentPart-1)
+			if (node.getName().strip()=="DB23"):
+				node.setnParts(2)
+				if currentPart is nPartsChicane:
+					myDipole_DH_A11=GeneralDipoleNoStrip("Dipole_DH_A11")
+					myDipole_DH_A11.setFunctionXPRigidity(deltaxp_rigidity)
+					myDipole_DH_A11.setFunctionXRigidity(deltax_rigidity)
+					myDipole_DH_A11.setFieldDirection(math.pi/2)
+					myDipole_DH_A11.setEffLength(theEffLength)					
+					node.addChildNode(myDipole_DH_A11,AccNode.BODY,0)				
+			if (node.getName().strip()=="DH_A12"):
+				node.setnParts(numberOfParts_DH_A12)
+				print "total length= ",node.getLength()
+				print "segment length= ",node.getLength(3)
+				#myDipole_DH_A12=YDipole("Dipole_DH_A12")
+				myDipole_DH_A12=GeneralDipoleNoStrip("Dipole_DH_A12")
+				myDipole_DH_A12.setFunctionXPRigidity(deltaxp_rigidity)
+				myDipole_DH_A12.setFunctionXRigidity(deltax_rigidity)
+				myDipole_DH_A12.setFieldDirection(math.pi/2)
+				myDipole_DH_A12.setEffLength(theEffLength)			
+				node.addChildNode(myDipole_DH_A12,AccNode.BODY,3)
+		
+	#Turn off injection kickers
+	
+	strength_hkicker10 = 0
+	strength_hkicker13 = strength_hkicker10
+	strength_hkicker11 = 0
+	strength_hkicker12 = strength_hkicker11
+	strength_vkicker10 = 0
+	strength_vkicker13 = strength_vkicker10
+	strength_vkicker11 = 0
+	strength_vkicker12 = strength_vkicker11	
+	
+	kickerwave = flatTopWaveform(1.0)
+	
+	nodes = teapot_latt.getNodes()
+	hkick10 = nodes[10]
+	vkick10 = nodes[12]
+	hkick11	= nodes[14]
+	vkick11 = nodes[16]
+	vkick12 = nodes[49]
+	hkick12 = nodes[51]
+	vkick13 = nodes[53]
+	hkick13	= nodes[55]
+	
+	vkick10.setParam("ky", strength_vkicker10)
+	hkick10.setParam("kx", strength_hkicker10)
+	vkick11.setParam("ky", strength_vkicker11)
+	hkick11.setParam("kx", strength_hkicker11)
+	vkick12.setParam("ky", strength_vkicker12)
+	hkick12.setParam("kx", strength_hkicker12)
+	vkick13.setParam("ky", strength_vkicker13)
+	hkick13.setParam("kx", strength_hkicker13)
+	
+	vkick10.setWaveform(kickerwave)
+	hkick10.setWaveform(kickerwave)
+	vkick11.setWaveform(kickerwave)
+	hkick11.setWaveform(kickerwave)
+	vkick12.setWaveform(kickerwave)
+	hkick12.setWaveform(kickerwave)
+	vkick13.setWaveform(kickerwave)
+	hkick13.setWaveform(kickerwave)
+	
+	teapot_latt.initialize()
+	
+	
+	
+	
+	
+	
+	
+	#scorer = MyScorer(0.005769,0.002069,0.001778,-0.000359,-0.003845,0.000000)
+	#scorer = MyScorer(0.004334,0.000192,0.001710,-0.000349,-0.004286,0.000000)
+	#scorer = MyScorer(0.000000,0.000000,0.000000,0.000000,0.000000,0.000000)
+	
+	scorer = MyScorer(teapot_latt)
+	scorer.initChicanes()
+	#searchAlgorithm   = RandomSearchAlgorithm()
+	searchAlgorithm = SimplexSearchAlgorithm()
+	
+	#max_time = 0.05
+	max_time = 10
+	max_accuracy=1E-10
+	max_iterations=1000
+	
+	iterationStopper=SolveStopperFactory.maxIterationStopper(max_iterations)
+	timeStopper = SolveStopperFactory.maxTimeStopper(max_time)
+	solverStopper=SolveStopperFactory.comboStopper()
+	solverStopper.addStopper(iterationStopper)
+	solverStopper.addStopper(timeStopper)
+	#solverStopper = SolveStopperFactory.maxAccuracyStopper(max_accuracy)
+	
+	solver = Solver()
+	solver.setAlgorithm(searchAlgorithm)
+	solver.setStopper(solverStopper)
+	
+	trialPoint = TrialPoint()
+	#trialPoint.addVariableProxy(VariableProxy(name = "x0", value = 1., step = 0.1))
+	trialPoint.addVariableProxy(VariableProxy(name = "x0", value = 1., step = 0.1))
+	trialPoint.addVariableProxy(VariableProxy(name = "x1", value = 1., step = 0.1))
+	trialPoint.addVariableProxy(VariableProxy(name = "x2", value = 1., step = 0.1))
+	trialPoint.addVariableProxy(VariableProxy(name = "x3", value = 1., step = 0.1))
+	x0 = trialPoint.getVariableProxyArr()[0]
+	x1 = trialPoint.getVariableProxyArr()[1]
+	x2 = trialPoint.getVariableProxyArr()[2]
+	x3 = trialPoint.getVariableProxyArr()[3]
+	
+	#x0.setUseInSolver(False)
+	solver.solve(scorer,trialPoint)
+	
+	print "===== best score ========== fitting time = ", solver.getScoreboard().getRunTime()
+	
+	bestScore = solver.getScoreboard().getBestScore()	
+	print "best score=",bestScore," iteration=",solver.getScoreboard().getIteration()
+	
+	trialPoint = solver.getScoreboard().getBestTrialPoint()
+	
+	print trialPoint.textDesciption()
+	
+	print "(%f,%f,%f,%f)"%(trialPoint.getVariableProxyValuesArr()[0],trialPoint.getVariableProxyValuesArr()[1],trialPoint.getVariableProxyValuesArr()[2],trialPoint.getVariableProxyValuesArr()[3])
+	outputDirectory="WasteBeamClosed"
+	fileOut=open("%s/ChicaneScales_%d.txt"%(outputDirectory,currentPart),'w')
+	fileOut.write("%f,%f,%f,%f"%(trialPoint.getVariableProxyValuesArr()[0],trialPoint.getVariableProxyValuesArr()[1],trialPoint.getVariableProxyValuesArr()[2],trialPoint.getVariableProxyValuesArr()[3]) +"\n")
+	fileOut.flush() 
+	fileOut.close() 
+
